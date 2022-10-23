91aecbfd4d3bf307317cd8de5a462afddfc9bebd
shin1 <gci787@naver.com>
2022-10-23 20:47:38 +0900

    신고 내용 bean

---
+++ portfolio/portfolio1/ReportBean.java
@@ -0,0 +1,47 @@
+package portfolio1;
+
+public class ReportBean {
+	private int num;
+	private int rNum;
+	private int boardType;
+	private String reportType;
+	private String comment;
+	private String write_date;
+	
+	public int getNum() {
+		return num;
+	}
+	public void setNum(int num) {
+		this.num = num;
+	}
+	public int getrNum() {
+		return rNum;
+	}
+	public void setrNum(int rNum) {
+		this.rNum = rNum;
+	}
+	public int getBoardType() {
+		return boardType;
+	}
+	public void setBoardType(int boardType) {
+		this.boardType = boardType;
+	}
+	public String getReportType() {
+		return reportType;
+	}
+	public void setReportType(String reportType) {
+		this.reportType = reportType;
+	}
+	public String getComment() {
+		return comment;
+	}
+	public void setComment(String comment) {
+		this.comment = comment;
+	}
+	public String getWrite_date() {
+		return write_date;
+	}
+	public void setWrite_date(String write_date) {
+		this.write_date = write_date;
+	}
+}
