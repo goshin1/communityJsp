4d34affb29f8ef2188e348bd1842086ec59ba041
shin1 <gci787@naver.com>
2022-10-23 20:47:28 +0900

    채용 게시글 bean

---
+++ portfolio/portfolio1/JobBoardBean.java
@@ -0,0 +1,98 @@
+package portfolio1;
+
+public class JobBoardBean {
+	private int num;
+	private String job;
+	private String degree;
+	private String exp;
+	private String main_area;
+	private String sub_area;
+	private String company;
+	
+	private String writer;
+	private String subject;
+	private String content;
+	private String file_name;
+	private int file_size;
+	private String write_date;
+	
+	public int getNum() {
+		return num;
+	}
+	public void setNum(int num) {
+		this.num = num;
+	}
+	public String getJob() {
+		return job;
+	}
+	public void setJob(String job) {
+		this.job = job;
+	}
+	public String getDegree() {
+		return degree;
+	}
+	public void setDegree(String degree) {
+		this.degree = degree;
+	}
+	public String getExp() {
+		return exp;
+	}
+	public void setExp(String exp) {
+		this.exp = exp;
+	}
+	public String getMain_area() {
+		return main_area;
+	}
+	public void setMain_area(String main_area) {
+		this.main_area = main_area;
+	}
+	public String getSub_area() {
+		return sub_area;
+	}
+	public void setSub_area(String sub_area) {
+		this.sub_area = sub_area;
+	}
+	public String getCompany() {
+		return company;
+	}
+	public void setCompany(String company) {
+		this.company = company;
+	}
+	public String getWriter() {
+		return writer;
+	}
+	public void setWriter(String writer) {
+		this.writer = writer;
+	}
+	public String getSubject() {
+		return subject;
+	}
+	public void setSubject(String subject) {
+		this.subject = subject;
+	}
+	public String getContent() {
+		return content;
+	}
+	public void setContent(String content) {
+		this.content = content;
+	}
+	public String getFile_name() {
+		return file_name;
+	}
+	public void setFile_name(String file_name) {
+		this.file_name = file_name;
+	}
+	public int getFile_size() {
+		return file_size;
+	}
+	public void setFile_size(int file_size) {
+		this.file_size = file_size;
+	}
+	public String getWrite_date() {
+		return write_date;
+	}
+	public void setWrite_date(String write_date) {
+		this.write_date = write_date;
+	}
+	
+}
