c23a332d38841e3976a6f070cb2cd7596ecee077
shin1 <gci787@naver.com>
2022-10-23 20:47:04 +0900

    관리 페이지 관련 기능 추가, 채용 게시판, 게시글 관련 기능 추가

--- portfolio/portfolio1/PortMgr.java
+++ portfolio/portfolio1/PortMgr.java
@@ -5,6 +5,7 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.Statement;
 import java.util.Vector;
+import java.util.Arrays;
 
 import javax.servlet.ServletRequest;
 
@@ -21,6 +22,338 @@ public class PortMgr {
 			e.printStackTrace();
 		}
 	}
+	// 관리자 계정 확인
+	public boolean checkManager(String id) {
+		Connection con = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		boolean b = false;
+		try {
+			String sql = "select * from manager where manager = ?";
+			con = pool.getConnection();
+			pstmt = con.prepareStatement(sql);
+			pstmt.setString(1, id);
+			rs = pstmt.executeQuery();
+			if(rs.next()) {
+				System.out.print(rs.getString("manager"));
+				b = true;
+			}
+			
+		}catch(Exception e) {
+			e.printStackTrace();
+		}finally {
+			pool.freeConnection(con, pstmt, rs);
+		}
+		return b;
+	}
+	
+	// 게시글 작성자 조회
+	public String selectWriter(int num) {
+		Connection con = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		String writer = null;
+		try {
+			String sql = "select * from boards where writer = ?";
+			con = pool.getConnection();
+			pstmt = con.prepareStatement(sql);
+			rs = pstmt.executeQuery();
+			if(rs.next())
+				writer = rs.getString("writer");
+		}catch(Exception e) {
+			e.printStackTrace();
+		}finally {
+			pool.freeConnection(con, pstmt, rs);
+		}
+		
+		return writer;
+	}
+	
+	
+	// 게시글 신고
+	public void insertReportBoard(int rNum, int boardType, String reportType, String comment) {
+		Connection con = null;
+		PreparedStatement pstmt = null;
+		try {
+			String sql = "insert into report_boards(rNum, boardType, reportType, comment) values(?, ?, ?, ?)";
+			con = pool.getConnection();
+			pstmt = con.prepareStatement(sql);
+			pstmt.setInt(1, rNum);
+			pstmt.setInt(2, boardType);
+			pstmt.setString(3, reportType);
+			pstmt.setString(4, comment);
+			pstmt.executeUpdate();
+		}catch(Exception e) {
+			e.printStackTrace();
+		}finally {
+			pool.freeConnection(con, pstmt);
+		}
+	}
+	
+	// 신고 게시판 글 가져오기
+	public Vector<ReportBean> getReportBoardList(){
+		Vector<ReportBean> v = new Vector<ReportBean>();
+		Connection con = null;
+		Statement stmt = null;
+		ResultSet rs = null;
+		try {
+			String sql = "select * from report_boards order by num desc";
+			con = pool.getConnection();
+			stmt = con.createStatement();
+			rs = stmt.executeQuery(sql);
+			while(rs.next()) {
+				ReportBean bean = new ReportBean();
+				bean.setNum(rs.getInt("num"));
+				bean.setrNum(rs.getInt("rNum"));
+				bean.setBoardType(rs.getInt("boardType"));
+				bean.setReportType(rs.getString("reportType"));
+				bean.setComment(rs.getString("comment"));
+				bean.setWrite_date(rs.getString("write_date"));;
+				v.add(bean);
+			}
+		}catch(Exception e) {
+			e.printStackTrace();
+		}finally {
+			pool.freeConnection(con, stmt, rs);
+		}		
+		return v;
+	}
+	
+	// 신고당한 게시글 내역 삭제
+	public void deleteReport(int num) {
+		Connection con = null;
+		PreparedStatement pstmt = null;
+		try {
+			String sql = "delete from report_boards where num = ?";
+			con = pool.createConnection();
+			pstmt = con.prepareStatement(sql);
+			pstmt.setInt(1, num);
+			pstmt.executeUpdate();
+		}catch(Exception e) {
+			e.printStackTrace();
+		}finally {
+			pool.freeConnection(con, pstmt);
+		}
+	}
+	
+	// 채용 게시판 글 가져오기
+	public Vector<JobBoardBean> getJobBoardList(String[] searchTypes, String[] jobs, String[] degs, String[] exps, String bArea, String[] sArea){
+		Vector<JobBoardBean> v = new Vector<JobBoardBean>();
+		Connection con = null;
+		Statement stmt = null;
+		ResultSet rs = null;
+		String sql = "";
+		
+		try {
+			sql = "select * from job_boards ";
+			if(Arrays.asList(searchTypes).contains("1")) {
+				sql = "select * from job_boards where ";
+				// 직무 
+				if(!searchTypes[0].equals("0")) {
+					if(sql.length() > 31)
+						sql += " and ";
+					sql += "job in (";
+					for(int i = 0; i < jobs.length - 1; i++) {
+						sql += "'"+jobs[i]+"', ";
+					}
+					sql += " '"+jobs[jobs.length - 1] + "')";
+				}
+				
+				// 학위
+				if(!searchTypes[1].equals("0")) {
+					if(sql.length() > 31)
+						sql += " and ";
+					sql += "degree in (";
+					for(int i = 0; i < degs.length - 1; i++) {
+						sql += "'"+degs[i]+"', ";
+					}
+					sql += " '"+degs[degs.length - 1] + "')";
+				}
+				
+				// 경력
+				if(!searchTypes[2].equals("0")) {
+					if(sql.length() > 31)
+						sql += " and ";
+					sql += "exp in (";
+					for(int i = 0; i < exps.length - 1; i++) {
+						sql += "'"+degs[i]+"', ";
+					}
+					sql += " '"+exps[exps.length - 1] + "')";
+				}
+				
+				// 지역 검색
+				if(!searchTypes[3].equals("0")) {
+					if(sql.length() > 31)
+						sql += " and ";
+					sql += "main_area = '"+bArea+"' and sub_area in (";
+					for(int i = 0; i < sArea.length - 1; i++) {
+						sql += "'"+sArea[i]+"', ";
+					}
+					sql += " '"+sArea[sArea.length - 1] + "')";
+				}
+				
+				
+				
+				
+			}
+			sql += " order by num desc";
+			con = pool.getConnection();
+			stmt = con.createStatement();
+			rs = stmt.executeQuery(sql);
+			while(rs.next()) {
+				JobBoardBean jBean = new JobBoardBean();
+				jBean.setNum(rs.getInt("num"));
+				jBean.setJob(rs.getString("job"));
+				jBean.setDegree(rs.getString("degree"));
+				jBean.setExp(rs.getString("exp"));
+				jBean.setMain_area(rs.getString("main_area"));
+				jBean.setSub_area(rs.getString("sub_area"));
+				jBean.setCompany(rs.getString("company"));
+				jBean.setWriter(rs.getString("writer"));
+				jBean.setSubject(rs.getString("subject"));
+				jBean.setContent(rs.getString("content"));
+				jBean.setFile_name(rs.getString("file_name"));
+				jBean.setFile_size(rs.getInt("file_size"));
+				jBean.setWrite_date(rs.getString("write_date"));
+				v.add(jBean);
+			}
+			
+		}catch(Exception e) {
+			e.printStackTrace();
+		}finally {
+			pool.freeConnection(con, stmt, rs);
+		}
+		return v;
+	}
+	
+	// 채용 게시글 작성
+	public int insertJobBoard(String job, String deg, String exp, String bArea, String sArea, String company, String writer, String subject, String content, String file_name, int file_size) {
+		Connection con = null;
+		PreparedStatement pstmt = null;
+		PreparedStatement pstmt2 = null;
+		ResultSet rs = null;
+		String sql = null;
+		int res = -1;
+		try {
+			con = pool.getConnection();
+			sql = "insert into job_boards(job, degree, exp, main_area, sub_area, company, writer, subject, content, file_name, file_size) "
+					+ "values(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
+			pstmt = con.prepareStatement(sql);
+			pstmt.setString(1, job);
+			pstmt.setString(2, deg);
+			pstmt.setString(3, exp);
+			pstmt.setString(4, bArea);
+			pstmt.setString(5, sArea);
+			pstmt.setString(6, company);
+			pstmt.setString(7, writer);
+			pstmt.setString(8, subject);
+			pstmt.setString(9, content);
+			pstmt.setString(10, file_name);
+			pstmt.setInt(11, file_size);
+			pstmt.executeUpdate();
+			pstmt.close();
+			
+			sql = "select max(num) from boards";
+			pstmt2 = con.prepareStatement(sql);
+			rs = pstmt2.executeQuery();
+			if(rs.next()) {
+				res = rs.getInt(1);
+			}
+		}catch(Exception e) {
+			e.printStackTrace();
+		}finally {
+			pool.freeConnection(con, pstmt2);
+		}
+		return res;
+	}
+	
+	// 채용 게시글 읽어오기
+	public JobBoardBean portJobContent(int num) {
+		Connection con = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		JobBoardBean bean = null;
+		try {
+			bean = new JobBoardBean();
+			String sql = "select * from job_boards where num = ?";
+			con = pool.getConnection();
+			pstmt = con.prepareStatement(sql);
+			pstmt.setInt(1, num);
+			rs = pstmt.executeQuery();
+			if(rs.next()) {
+				bean.setNum(rs.getInt("num"));
+				bean.setJob(rs.getString("job"));
+				bean.setDegree(rs.getString("degree"));
+				bean.setExp(rs.getString("exp"));
+				bean.setMain_area(rs.getString("main_area"));
+				bean.setSub_area(rs.getString("sub_area"));
+				bean.setCompany(rs.getString("company"));
+				bean.setWriter(rs.getString("writer"));
+				bean.setSubject(rs.getString("subject"));
+				bean.setContent(rs.getString("content"));
+				bean.setFile_name(rs.getString("file_name"));
+				bean.setFile_size(rs.getInt("file_size"));
+				bean.setWrite_date(rs.getString("write_date"));
+			}
+		}catch(Exception e) {
+			e.printStackTrace();
+		}finally {
+			pool.freeConnection(con, pstmt, rs);
+		}
+		
+		return bean;
+	}
+
+	// 채용 게시글 수정
+	public int modifyJobBoard(int num, String job, String deg, String exp, String bArea, String sArea, String company, String writer, String subject, String content, String file_name, int file_size) {
+		Connection con = null;
+		PreparedStatement pstmt = null;
+		int res = -1;
+		try {
+			String sql = "update job_boards set job=?, degree=?, exp=?, main_area=?, sub_area=?, company=?, subject=?, content=?, file_name=?, file_size=? where num=? and writer=?";
+			con = pool.getConnection();
+			pstmt = con.prepareStatement(sql);
+			pstmt.setString(1, job);
+			pstmt.setString(2, deg);
+			pstmt.setString(3, exp);
+			pstmt.setString(4, bArea);
+			pstmt.setString(5, sArea);
+			pstmt.setString(6, company);
+			pstmt.setString(7, subject);
+			pstmt.setString(8, content);
+			pstmt.setString(9, file_name);
+			pstmt.setInt(10, file_size);
+			pstmt.setInt(11, num);
+			pstmt.setString(12, writer);
+			res = pstmt.executeUpdate();
+		}catch(Exception e) {
+			e.printStackTrace();
+		}finally {
+			pool.freeConnection(con, pstmt);
+		}
+		return res;
+	}
+	
+	// 채용 게시글 삭제
+	public int deleteJobBoard(int num) {
+		Connection con = null;
+		PreparedStatement pstmt = null;
+		int res = -1;
+		try {
+			String sql = "delete from job_boards where num = ?";
+			con = pool.getConnection();
+			pstmt = con.prepareStatement(sql);
+			pstmt.setInt(1, num);
+			res = pstmt.executeUpdate();
+		}catch(Exception e) {
+			e.printStackTrace();
+		}finally {
+			pool.freeConnection(con, pstmt);
+		}
+		return res;
+	}
+	
+	
 	
 	// 게시글 목록 가져오기
 	public Vector<BoardBean> getBoardList(String keyWord){
@@ -31,7 +364,7 @@ public class PortMgr {
 		String sql = "";
 		
 		try {
-			if(keyWord.equals("") || keyWord == null) {
+			if(keyWord == null) {
 				sql = "select * from boards order by num desc";
 			} else {
 				sql = "select * from boards where content like '%" + keyWord + "%' or subject like '%" + keyWord + "%' order by num desc";
@@ -68,7 +401,6 @@ public class PortMgr {
 		}finally {
 			pool.freeConnection(con, stmt, rs);
 		}
-		
 		return v;
 	}
 	
@@ -146,7 +478,7 @@ public class PortMgr {
 	}
 	
 	// 게시글 작성
-	public int insertBoard(String writer, String subject, String content, String filename, int file_size) {
+	public int insertBoard(String writer, String subject, String content, String file_name, int file_size) {
 		Connection con = null;
 		PreparedStatement pstmt = null;
 		PreparedStatement pstmt2 = null;
@@ -155,12 +487,12 @@ public class PortMgr {
 		int res = -1;
 		try {
 			con = pool.getConnection();
-			sql = "insert boards(writer, subject, content, file_name, file_size, views, review) values(?, ?, ?, ?, ?, 0, 0)";
+			sql = "insert into boards(writer, subject, content, file_name, file_size, views, review) values(?, ?, ?, ?, ?, 0, 0)";
 			pstmt = con.prepareStatement(sql);
 			pstmt.setString(1, writer);
 			pstmt.setString(2, subject);
 			pstmt.setString(3, content);
-			pstmt.setString(4, filename);
+			pstmt.setString(4, file_name);
 			pstmt.setInt(5, file_size);
 			pstmt.executeUpdate();
 			pstmt.close();
@@ -171,9 +503,6 @@ public class PortMgr {
 			if(rs.next()) {
 				res = rs.getInt(1);
 			}
-			
-			
-			
 		}catch(Exception e) {
 			e.printStackTrace();
 		}finally {
@@ -181,6 +510,31 @@ public class PortMgr {
 		}
 		return res;
 	}
+	
+	// 게시글 수정
+	public int modifyBoard(int num, String writer, String subject, String content, String file_name, int file_size) {
+		Connection con = null;
+		PreparedStatement pstmt = null;
+		int res = -1;
+		try {
+			String sql = "update boards set subject=?, content=?, file_name=?, file_size=? where num = ? and writer = ?";
+			con = pool.getConnection();
+			pstmt = con.prepareStatement(sql);
+			pstmt.setString(1, subject);
+			pstmt.setString(2, content);
+			pstmt.setString(3, file_name);
+			pstmt.setInt(4, file_size);
+			pstmt.setInt(5, num);
+			pstmt.setString(6, writer);
+			res = pstmt.executeUpdate();
+		}catch(Exception e) {
+			e.printStackTrace();
+		}finally {
+			pool.freeConnection(con, pstmt);
+		}
+		return res;
+	}
+			
 	
 	// 게시글 삭제
 	public int deleteBoard(int num) {
@@ -204,6 +558,7 @@ public class PortMgr {
 		return res;
 	}
 	
+	// 가장 인기 있는 게시글
 	public BoardBean popularBoard() {
 		BoardBean bean = null;
 		Connection con = null;
@@ -401,22 +756,22 @@ public class PortMgr {
 	}
 	
 	// 댓글 수 감소
-		public void decreaseReview(int bNum) {
-			Connection con = null;
-			PreparedStatement pstmt = null;
-			try {
-				String sql = "update boards set review = review - 1 where num = ?";
-				con = pool.getConnection();
-				pstmt = con.prepareStatement(sql);
-				pstmt.setInt(1, bNum);
-				pstmt.executeUpdate();
+	public void decreaseReview(int bNum) {
+		Connection con = null;
+		PreparedStatement pstmt = null;
+		try {
+			String sql = "update boards set review = review - 1 where num = ?";
+			con = pool.getConnection();
+			pstmt = con.prepareStatement(sql);
+			pstmt.setInt(1, bNum);
+			pstmt.executeUpdate();
 				
-			}catch(Exception e) {
-				e.printStackTrace();
-			}finally {
-				pool.freeConnection(con, pstmt);
-			}
+		}catch(Exception e) {
+			e.printStackTrace();
+		}finally {
+			pool.freeConnection(con, pstmt);
 		}
+	}
 	
 	
 	// 로그인
@@ -464,7 +819,7 @@ public class PortMgr {
 			if(rs.getInt(1) > 0) {
 				return "overlap";
 			}
-			sql = "insert members values(?, ?, ?, ?)";
+			sql = "insert into members values(?, ?, ?, ?)";
 			pstmt2 = con.prepareStatement(sql);
 			pstmt2.setString(1, id);
 			pstmt2.setString(2, pwd);
@@ -491,7 +846,7 @@ public class PortMgr {
 			con = pool.getConnection();
 			stmt = con.createStatement();
 			for(int i = 0; i < 150; i++) {
-				sql = "insert boards(writer, subject, content, file_name, file_size, views, review) values('1111', '테스트"+i+"', 'content"+i+"','', 0, 0, 0)";
+				sql = "insert into boards(writer, subject, content, file_name, file_size, views, review) values('1111', '테스트"+i+"', 'content"+i+"','', 0, 0, 0)";
 				int check = stmt.executeUpdate(sql);
 				if (check != 0)
 					System.out.println("start sucess!");
